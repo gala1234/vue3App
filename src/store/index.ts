36a3433d9026fe1a9923ec7596adb5833473ddef
gala1234 <galadriel.calero@gig.com>
2020-02-10 11:59:03 +0100

    login in store basic state

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 
 import { tasks } from './tasks'
 import { users } from './users'
+import { login } from './login'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
     tasks,
-    users
+    users,
+    login
   }
 })
---
+++ src/store/login/index.ts
@@ -0,0 +1,9 @@
+// import { actions } from './loging.actions'
+// import { getters } from './loging.getters'
+// import { mutations } from './loging.mutations'
+import { state } from './login.state'
+
+export const login = {
+  namespaced: false,
+  state
+}
